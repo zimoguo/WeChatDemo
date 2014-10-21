82cc67c9aa36bb4f0e818fa5846c26bcec81f398
Thea <943665622@qq.com>
2014-10-21 15:15:14 +0800

    second commit

---
+++ WeChatDemo/src/com/lili/guo/wechatdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.lili.guo.wechatdemo;
+
+import java.lang.reflect.Field;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.ViewConfiguration;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		setOverflowShowingAlways();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * ÆÁ±ÎÎïÀímenu¼ü
+	 */
+	private void setOverflowShowingAlways() {
+		try {
+			ViewConfiguration config = ViewConfiguration.get(this);
+			Field menuKeyField = ViewConfiguration.class
+					.getDeclaredField("sHasPermanentMenuKey");
+			menuKeyField.setAccessible(true);
+			menuKeyField.setBoolean(config, false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
